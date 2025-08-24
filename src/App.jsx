938e37f23dd453351173cc7c1312029ca3540cd3
Abdullah <95abdullah99@gmail.com>
2025-08-24 16:54:59 +0530

    Integrate GSAP and Tailwind CSS into the project; add Navbar and HeroSection components. Update package dependencies in package.json and package-lock.json to include @gsap/react, gsap, @tailwindcss/vite, and react-responsive. Implement basic styling and layout in index.css.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import Navbar from "./Components/Navbar"
+import HeroSection from "./Sections/HeroSection"
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <main>
+      <Navbar/>
+      <HeroSection/>
+      <div className="h-dvh border border-red-500"></div>
+    </main>
   )
 }
 
